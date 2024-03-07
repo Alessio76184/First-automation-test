ecafdb5097ef1236cac162ebb680bca54fdc8ff5
Alessio76184 <159435390+Alessio76184@users.noreply.github.com>
2024-03-07 23:07:24 +0200

    Expanding Test with post
    I changed the Get and Post functions as errors appeared, breaking the tests.
    
    Tried to figure out Post but got stuck with due to the Get & Post Errors

--- client/Python Tests/connection_manager.py
+++ client/Python Tests/connection_manager.py
@@ -3,20 +3,20 @@ baseUrl = "http://localhost:8080/"
  
 # handles the calls 
 def make_get_call(URL):
-    response = requests.get(baseUrl+URL)
-    ## this collects data from json to python
-    return response.json()
+    response = requests.get(baseUrl + URL)
+    return response
 
 def make_post_call(URL, body):
-    pass
-
-# Counting the number of questions within the questionnair
+    response = requests.post(baseUrl + URL, json=body)
+    return response  # Return the response object instead of JSON dat
+    
+# Counting the number of questions within the questionnaire
 def QuestionnaireIdCount(questions):
     id_count = 0
-    ## checking for all questions with 'id' to confirm the number of questions
-    for question in questions.get('questions', []):
+    # Convert the response to JSON and then access the 'questions' attribute
+    json_data = questions.json()
+    for question in json_data.get('questions', []):
         if question.get('id') is not None:
             id_count += 1
     return id_count
-
 
